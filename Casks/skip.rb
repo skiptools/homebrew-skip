2d5a3d57c7ba959d731578d5dbdca121a8151dda
marcprux <mwp1@cornell.edu>
2023-08-16 10:25:07 -0400

    Release 0.5.78

--- Casks/skip.rb
+++ Casks/skip.rb
@@ -1,18 +1,20 @@
 cask "skip" do
-  version "0.5.75"
-  sha256 "f1887c78214992ae288c137b8eb7e450db7a2a08b5fc146114f1aded2a3eaeb3"
+  version "0.5.78"
+  sha256 "be60f991736b60f10fb01a055ee014efd9f9fa40c2565423055ff9dcd75d2c84"
 
-  url "https://github.com/skiptools/skip/releases/download/#{version}/skip.zip",
+  url "https://github.com/skiptools/skip/releases/download/#{version}/skipstone.plugin.zip",
       verified: "github.com/skiptools/skip/"
   name "Skip"
   desc "Tool for creating dual-platform iPhone/Android apps in Swift"
   homepage "https://skip.tools/"
 
   depends_on formula: "gradle"
+  #depends_on cask: "android-studio"
+  depends_on macos: ">= :mojave"
 
-  binary "skip"
+  binary "skipstone.artifactbundle/skipstone"
 
   postflight do
-    system "xattr", "-r", "-d", "com.apple.quarantine", "#{staged_path}/skip"
+    system "xattr", "-r", "-d", "com.apple.quarantine", "#{staged_path}/skipstone.artifactbundle/skipstone"
   end
 end
