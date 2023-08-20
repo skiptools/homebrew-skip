d96750c11a51e4d5e01a9fd038c1d53a83246b08
marcprux <mwp1@cornell.edu>
2023-08-20 13:45:08 -0400

    Release 0.5.90

--- Casks/skip.rb
+++ Casks/skip.rb
@@ -1,16 +1,15 @@
 cask "skip" do
-  version "0.5.89"
-  sha256 "e635676bd204f020e430ae0a3e0a8355c3ca6a54d9efaa117dad27ff17f02be2"
+  version "0.5.90"
+  sha256 "ccf192649e1e80d3f758de717a33bf1152e4bbf7fc228c0ac61b1052d0540222"
 
-  url "https://github.com/skiptools/skip/releases/download/#{version}/skip.zip",
-      verified: "github.com/skiptools/skip/"
+  url "https://source.skip.tools/skip/releases/download/#{version}/skip.zip",
+      verified: "source.skip.tools/skip/"
   name "Skip"
   desc "Tool for creating dual-platform iPhone/Android apps in Swift"
   homepage "https://skip.tools/"
 
   depends_on formula: "gradle"
-  #depends_on cask: "android-studio"
-  depends_on macos: ">= :mojave"
+  depends_on macos: ">= :ventura"
 
   binary "skip"
 
