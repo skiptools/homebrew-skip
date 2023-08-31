d0087112c7abb5a89de0168faa60f1077851d436
marcprux <mwp1@cornell.edu>
2023-08-31 15:41:56 -0400

    Release 0.6.8

--- Casks/skip.rb
+++ Casks/skip.rb
@@ -1,6 +1,6 @@
 cask "skip" do
-  version "0.6.7"
-  sha256 "7d4c15fa232754c6a68d4437bc54535fff8761ead8f5edda6ba88b5fb81ea68c"
+  version "0.6.8"
+  sha256 "58faafa032972c8e2d70bf2a793cd698ddfc0f46212876d94b2490a5c9dc3e9e"
 
   url "https://source.skip.tools/skip/releases/download/#{version}/skip.zip",
       verified: "source.skip.tools/skip/"
