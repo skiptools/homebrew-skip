668a5ba8296ced3d54bbd8ccf80ca952a9c01ebd
marcprux <mwp1@cornell.edu>
2023-09-01 17:59:46 -0400

    Release 0.6.10

--- Casks/skip.rb
+++ Casks/skip.rb
@@ -1,6 +1,6 @@
 cask "skip" do
-  version "0.6.9"
-  sha256 "cf040f172547d02c3ce5b0f18184e1816a33324dbb5924cb6855acd0a1a9ffc4"
+  version "0.6.10"
+  sha256 "d548b1e7a24b93d5c6bdf5f7c7af6c450d593bb09456e75ea8e5c24725744919"
 
   url "https://source.skip.tools/skip/releases/download/#{version}/skip.zip",
       verified: "source.skip.tools/skip/"
@@ -12,15 +12,9 @@ cask "skip" do
   depends_on macos: ">= :ventura"
 
   binary "skip"
-  zap trash: "~/.skiptools"
 
   postflight do
     system "xattr", "-c", "#{staged_path}/skip"
+    system "#{staged_path}/skip", "welcome"
   end
-
-  caveats do
-    free_license "https://eval.skip.tools"
-    license "https://skip.tools/eula"
-  end
-
 end
