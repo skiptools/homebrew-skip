4565ee02b5022e63cbddaf2b637b361a6fca3c7e
marcprux <mwp1@cornell.edu>
2023-10-27 17:36:06 -0400

    Release 0.7.11

--- Casks/skip.rb
+++ Casks/skip.rb
@@ -1,6 +1,6 @@
 cask "skip" do
-  version "0.7.10"
-  sha256 "350d35747193f59e9dfba4b1665cb7a6e6f117a8e17bd843c52432bf0a5d6df2"
+  version "0.7.11"
+  sha256 "6acedf9d9ecbd84adbbe0fe8e2724e6e1c3049ed67668607f22e7bb45b9b31b6"
 
   url "https://source.skip.tools/skip/releases/download/#{version}/skip.zip",
       verified: "source.skip.tools/skip/"
@@ -9,9 +9,9 @@ cask "skip" do
   homepage "https://skip.tools/"
 
   depends_on formula: "gradle"
+  depends_on formula: "openjdk@17"
   depends_on cask: "android-platform-tools"
   depends_on cask: "android-commandlinetools"
-  depends_on cask: "openjdk@17"
   depends_on macos: ">= :ventura"
 
   binary "skip.artifactbundle/macos/skip"
