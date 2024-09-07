245c3ba8a9e26f97129d49fe638990fdb525de7b
Marc Prud'hommeaux <marc@skip.tools>
2024-09-07 09:43:27 -0400

    Add toolchain casks

---
+++ Casks/swift-android-toolchain@5.10.1.rb
@@ -0,0 +1,20 @@
+cask "swift-android-toolchain@5.10.1" do
+  version "5.10.1"
+  sha256 "015d304c12f77b9ea109c70ed8673a0c07109cc7b26069532f997069516a3762"
+
+  url "https://github.com/skiptools/swift-android-toolchain/releases/download/#{version}/swift-#{version}-android-sdk.tar.xz"
+  name "swift-android-toolchain@#{version}"
+  desc "Swift Android Toolchain"
+  homepage "https://skip.tools"
+
+  depends_on macos: ">= :ventura"
+  depends_on cask: "skiptools/skip/skip"
+  depends_on cask: "skiptools/skip/swift-host-toolchain@#{version}"
+
+  postflight do
+    puts "Creating toolchain link at ~/Library/Developer/Skip/SDKs/swift-#{version}-android-sdk"
+    target = Pathname.new("~/Library/Developer/Skip/SDKs/swift-#{version}-android-sdk").expand_path
+    FileUtils.mkdir_p target
+    File.symlink("#{staged_path}/swift-#{version}-android-sdk/usr", "#{target}/usr")  
+  end
+end
