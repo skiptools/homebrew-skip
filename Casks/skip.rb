5434f083bc23d29a6bc26440b402d00101d9722d
Marc Prud'hommeaux <marc@skip.tools>
2024-09-30 23:36:55 -0400

    Release 1.1.6

--- Casks/skip.rb
+++ Casks/skip.rb
@@ -1,6 +1,6 @@
 cask "skip" do
-  version "1.1.5"
-  sha256 "a34697633ea3dfee405b5c6bb989c6ae49febf770acd699f015ff1225a106a52"
+  version "1.1.6"
+  sha256 "a84134208db1377323585aaed24f754f30cb3c3af59f44675e6cdbfaa94fe28e"
 
   url "https://source.skip.tools/skip/releases/download/#{version}/skip.zip",
       verified: "source.skip.tools/skip/"
