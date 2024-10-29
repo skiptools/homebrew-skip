a6146089eea78752f148ad0e05c21ff459bfc893
Marc Prud'hommeaux <marc@skip.tools>
2024-10-29 10:42:53 -0400

    Add swift-host-toolchain@6.0.2.rb

--- Casks/swift-host-toolchain@6.0.2.rb
+++ Casks/swift-host-toolchain@6.0.2.rb
@@ -18,7 +18,9 @@ cask "swift-host-toolchain@6.0.2" do
   # this tries to install for all users by default, which requires sudo
   #pkg "swift-#{version}-RELEASE-osx.pkg"
 
-  # this installs into ~/Library/Developer/Toolchains which does not need sudo
+  # This installs into ~/Library/Developer/Toolchains which does not need sudo
+  # Homebrew won't support local packages wth the standard pkg: stanza
+  # See: https://github.com/Homebrew/brew/pull/14393
   installer script: {
     executable: "/usr/sbin/installer",
     args:       ["-pkg", "#{staged_path}/swift-#{version}-RELEASE-osx.pkg", "-target", "CurrentUserHomeDirectory"]
@@ -26,7 +28,8 @@ cask "swift-host-toolchain@6.0.2" do
 
   swift_package = Pathname.new("~/Library/Developer/Toolchains/swift-#{version}-RELEASE.xctoolchain/usr/bin/swift-package").expand_path
 
-  uninstall pkgutil: "org.swift.600202408011a"
+  # this only works for globally installed packages not CurrentUserHomeDirectory
+  #uninstall pkgutil: "org.swift.6020202410241a"
 
   # Documentation: https://docs.brew.sh/Cask-Cookbook#stanza-zap
   #zap trash: ""
