a5f1ce101370ee760019b3e2d06c7963ce06daf5
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-05-03 10:34:53 -0400

    Add android-commandlinetools to package dependencies

--- Casks/skip.rb
+++ Casks/skip.rb
@@ -11,6 +11,7 @@ cask "skip" do
   depends_on formula: "openjdk"
   depends_on formula: "gradle"
   depends_on cask: "android-platform-tools"
+  depends_on cask: "android-commandlinetools"
   depends_on macos: ">= :ventura"
 
   binary "skip.artifactbundle/macos/skip"
