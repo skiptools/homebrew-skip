022a6c3d367d3b38d7af9f52d42100c64fb95555
Abe White <abewhite@mac.com>
2025-05-06 11:32:36 -0500

    Release 1.5.9

--- Casks/skip.rb
+++ Casks/skip.rb
@@ -1,6 +1,6 @@
 cask "skip" do
-  version "1.5.8"
-  sha256 "b7ad4e4403fcd965f9ad040ee723c6b42f19dbd6f2eaff6887ee00d431e5c3cc"
+  version "1.5.9"
+  sha256 "46cabe0d8026b9fc23cb7506800d00248b60641791810bee0c280181a282e406"
 
   url "https://source.skip.tools/skip/releases/download/#{version}/skip.zip",
       verified: "source.skip.tools/skip/"
