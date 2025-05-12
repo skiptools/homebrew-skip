25ef2e78e1146227feaa850b8ced9c4198aacaf3
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-05-12 18:26:00 -0400

    Add nightly-6.2 casks

---
+++ Casks/swift-android-toolchain@nightly-6.2.rb
@@ -0,0 +1,35 @@
+cask "swift-android-toolchain@nightly-6.2" do
+  # note: Casks do not support aliases and symbolic links require exact cask
+  # name match, so new versions (e.g., swift-android-toolchain@6.1.x.rb)
+  # will need to be manually copied over to swift-android-toolchain@6.1.rb
+  version "nightly-6.2"
+  sha256 "77bc7bc8f08b4c265c2fbbe155ab7d029e223581e420ae84717ca415bdff6726"
+
+  # https://github.com/skiptools/swift-android-toolchain/releases/download/6.2-DEVELOPMENT-SNAPSHOT-2025-05-07-a/swift-6.2-DEVELOPMENT-SNAPSHOT-2025-05-07-a-android-0.1.artifactbundle.tar.gz
+  swift_version "6.2-DEVELOPMENT-SNAPSHOT-2025-05-07-a"
+
+  #artifact = "swift-#{version}-RELEASE-android-24-0.1.artifactbundle"
+  artifact "swift-#{swift_version}-android-0.1.artifactbundle"
+
+  url "https://source.skip.tools/swift-android-toolchain/releases/download/#{swift_version}/#{artifact}.tar.gz"
+  name "swift-android-toolchain@#{version}"
+  desc "Swift Android Toolchain"
+  homepage "https://skip.tools"
+
+  depends_on cask: "skiptools/skip/swift-host-toolchain@#{version}"
+  depends_on cask: "skiptools/skip/skip"
+  depends_on macos: ">= :ventura"
+
+  swiftcmd = Pathname.new("~/Library/Developer/Toolchains/swift-#{version}-RELEASE.xctoolchain/usr/bin/swift").expand_path
+
+  postflight do
+    system "xattr", "-d", "-r", "-s", "com.apple.quarantine", "#{staged_path}/#{artifact}"
+    system "#{swiftcmd}", "sdk", "install", "#{staged_path}/#{artifact}"
+  end
+
+  uninstall_preflight do
+    system "#{swiftcmd}", "sdk", "remove", "swift-#{version}-RELEASE-android-24-0.1"
+  end
+
+  #uninstall delete: "~/Library/Developer/Skip/SDKs/swift-#{version}-RELEASE-android-sdk"
+end
---
+++ Casks/swift-host-toolchain@nightly-6.2.rb
@@ -0,0 +1,39 @@
+cask "swift-host-toolchain@nightly-6.2" do
+  version "nightly-6.2"
+  sha256 "f03a82e47984e727073dbdaa42b9445ae278f7b5c19a31a870b738dd768d60a4"
+
+  swift_release "swift-6.2-DEVELOPMENT-SNAPSHOT-2025-05-07-a"
+
+  #url "https://download.swift.org/swift-#{version}-release/xcode/swift-#{version}-RELEASE/swift-#{version}-RELEASE-osx.pkg"
+  url "https://download.swift.org/swift-6.2-branch/xcode/#{swift_release}/#{swift_release}-osx.pkg"
+  name "swift-#{version}"
+  desc "Swift Open Source Xcode Toolchain"
+  homepage "https://swift.org"
+
+  # Documentation: https://docs.brew.sh/Brew-Livecheck
+  #livecheck do
+  #  url ""
+  #  strategy ""
+  #end
+
+  depends_on macos: ">= :ventura"
+
+  # this tries to install for all users by default, which requires sudo
+  #pkg "swift-#{version}-RELEASE-osx.pkg"
+
+  # This installs into ~/Library/Developer/Toolchains which does not need sudo
+  # Homebrew won't support local packages wth the standard pkg: stanza
+  # See: https://github.com/Homebrew/brew/pull/14393
+  installer script: {
+    executable: "/usr/sbin/installer",
+    args:       ["-pkg", "#{staged_path}/#{swift_release}-osx.pkg", "-target", "CurrentUserHomeDirectory"]
+  }
+
+  swift_package = Pathname.new("~/Library/Developer/Toolchains/#{swift_release}.xctoolchain/usr/bin/swift-package").expand_path
+
+  # this only works for globally installed packages not CurrentUserHomeDirectory
+  #uninstall pkgutil: "org.swift.6020202410241a"
+
+  # Documentation: https://docs.brew.sh/Cask-Cookbook#stanza-zap
+  #zap trash: ""
+end
