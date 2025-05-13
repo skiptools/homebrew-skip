b49d15c0caccf91a35ebb3849d35569e2e0ca06f
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-05-12 21:39:16 -0400

    Update Android toolchain nightly-6.2 script

--- Casks/swift-android-toolchain@nightly-6.2.rb
+++ Casks/swift-android-toolchain@nightly-6.2.rb
@@ -25,11 +25,18 @@ cask "swift-android-toolchain@nightly-6.2" do
   sdkpath = Pathname.new("~/Library/org.swift.swiftpm/#{artifact}").expand_path
 
   postflight do
-          system_command "/usr/bin/killall",
-                   args:         ["coreaudiod"],
-                   sudo:         true,
-                   must_succeed: true
-
+    system_command "xattr",
+        args: ["-d", "-r", "-s", "com.apple.quarantine", "#{staged_path}/#{artifact}"],
+        sudo: false,
+        must_succeed: true
+    system_command "#{swiftcmd}",
+        args: ["sdk", "install", "#{staged_path}/#{artifact}"],
+        sudo: false,
+        must_succeed: true
+    system_command "#{sdkpath}/swift-android/scripts/setup-android-sdk.sh"],
+        args: [],
+        sudo: false,
+        must_succeed: true
   end
 
   uninstall_preflight do
