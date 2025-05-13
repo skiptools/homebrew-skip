ebd0715bfc1035808d28e78ecec064de17b65cf0
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-05-12 21:37:29 -0400

    Update Android toolchain nightly-6.2 script

--- Casks/swift-android-toolchain@nightly-6.2.rb
+++ Casks/swift-android-toolchain@nightly-6.2.rb
@@ -26,13 +26,10 @@ cask "swift-android-toolchain@nightly-6.2" do
 
   postflight do
     system_command "xattr",
-        args: ["-d", "-r", "-s", "com.apple.quarantine", "#{staged_path}/#{artifact}"],
         must_succeed: true
     system_command "#{swiftcmd}",
-        args: ["sdk", "install", "#{staged_path}/#{artifact}"],
         must_succeed: true
     system_command "#{sdkpath}/swift-android/scripts/setup-android-sdk.sh"],
-        args: [],
         must_succeed: true
   end
 
