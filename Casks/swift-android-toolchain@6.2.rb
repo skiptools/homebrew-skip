b7890811c1e287c5275f35094f06e7d4d7457cf6
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-09-16 15:56:56 -0400

    Add Swift 6.2 host toolchain and Android SDK

---
+++ Casks/swift-android-toolchain@6.2.rb
@@ -0,0 +1,46 @@
+cask "swift-android-toolchain@6.2" do
+  # note: Casks do not support aliases and symbolic links require exact cask
+  # name match, so new versions (e.g., swift-android-toolchain@6.1.x.rb)
+  # will need to be manually copied over to swift-android-toolchain@6.1.rb
+  version "6.2"
+  sha256 "52cfebf048b8fe732403c98474908a3f237913cbba9e3fcef3f90ee732bf2cec"
+
+  # https://github.com/skiptools/swift-android-toolchain/releases/download/6.2-DEVELOPMENT-SNAPSHOT-2025-08-28-a/swift-6.2-DEVELOPMENT-SNAPSHOT-2025-08-28-a-android-0.1.artifactbundle.tar.gz
+  swift_version = "6.2"
+
+  #artifact = "swift-#{version}-RELEASE-android-24-0.1.artifactbundle"
+  swift_android = "swift-#{swift_version}-android-0.1"
+  artifact = "#{swift_android}.artifactbundle"
+
+  url "https://source.skip.tools/swift-android-toolchain/releases/download/#{swift_version}/#{artifact}.tar.gz"
+  name "swift-android-toolchain@#{version}"
+  desc "Swift Android Toolchain"
+  homepage "https://skip.tools"
+
+  depends_on cask: "skiptools/skip/swift-host-toolchain@#{version}"
+  depends_on cask: "skiptools/skip/skip"
+  depends_on cask: "android-ndk"
+  depends_on macos: ">= :ventura"
+
+  swiftcmd = Pathname.new("~/Library/Developer/Toolchains/swift-#{swift_version}.xctoolchain/usr/bin/swift").expand_path
+  sdkpath = Pathname.new("~/Library/org.swift.swiftpm/swift-sdks/#{artifact}").expand_path
+
+  postflight do
+    system_command "xattr",
+        args: ["-d", "-r", "-s", "com.apple.quarantine", "#{staged_path}/#{artifact}"],
+        must_succeed: true
+    system_command "#{swiftcmd}",
+        args: ["sdk", "install", "#{staged_path}/#{artifact}"],
+        must_succeed: true
+
+    system_command "env",
+        args: ["ANDROID_NDK_HOME=#{HOMEBREW_PREFIX}/share/android-ndk", "#{sdkpath}/swift-android/scripts/setup-android-sdk.sh"],
+        must_succeed: true
+  end
+
+  uninstall_preflight do
+    system "#{swiftcmd}", "sdk", "remove", "#{swift_android}"
+  end
+
+  #uninstall delete: "~/Library/Developer/Skip/SDKs/swift-#{version}-RELEASE-android-sdk"
+end
---
+++ Casks/swift-host-toolchain@6.2.rb
@@ -0,0 +1,38 @@
+cask "swift-host-toolchain@6.2" do
+  version "6.2"
+  sha256 "9eac4a13500c7a6dedfd90814fd31c17af2efc38150137b56b70bf5f44b2d64b"
+
+  swift_release = "swift-6.2"
+
+  url "https://download.swift.org/swift-#{version}-release/xcode/swift-#{version}-RELEASE/swift-#{version}-RELEASE-osx.pkg"
+  name "swift-#{version}"
+  desc "Swift Open Source Xcode Toolchain"
+  homepage "https://swift.org"
+
+  # Documentation: https://docs.brew.sh/Brew-Livecheck
+  #livecheck do
+  #  url ""
+  #  strategy ""
+  #end
+
+  depends_on macos: ">= :ventura"
+
+  # this tries to install for all users by default, which requires sudo
+  #pkg "swift-#{version}-RELEASE-osx.pkg"
+
+  # This installs into ~/Library/Developer/Toolchains which does not need sudo
+  # Homebrew won't support local packages wth the standard pkg: stanza
+  # See: https://github.com/Homebrew/brew/pull/14393
+  installer script: {
+    executable: "/usr/sbin/installer",
+    args:       ["-pkg", "#{staged_path}/#{swift_release}-osx.pkg", "-target", "CurrentUserHomeDirectory"]
+  }
+
+  swift_package = Pathname.new("~/Library/Developer/Toolchains/#{swift_release}.xctoolchain/usr/bin/swift-package").expand_path
+
+  # this only works for globally installed packages not CurrentUserHomeDirectory
+  #uninstall pkgutil: "org.swift.6020202410241a"
+
+  # Documentation: https://docs.brew.sh/Cask-Cookbook#stanza-zap
+  #zap trash: ""
+end
