fa978b3910a2a3c9c2c6cfdfdb94bcf781c52bfa
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-09-16 16:14:01 -0400

    Update Swift 6.2 host toolchain and Android SDK

--- Casks/swift-android-toolchain@6.2.rb
+++ Casks/swift-android-toolchain@6.2.rb
@@ -7,9 +7,10 @@ cask "swift-android-toolchain@6.2" do
 
   # https://github.com/skiptools/swift-android-toolchain/releases/download/6.2-DEVELOPMENT-SNAPSHOT-2025-08-28-a/swift-6.2-DEVELOPMENT-SNAPSHOT-2025-08-28-a-android-0.1.artifactbundle.tar.gz
   swift_version = "6.2"
+  swift_version_id = "#{swift_version}-RELEASE"
 
   #artifact = "swift-#{version}-RELEASE-android-24-0.1.artifactbundle"
-  swift_android = "swift-#{swift_version}-RELEASE-android-0.1"
+  swift_android = "swift-#{swift_version_id}-android-0.1"
   artifact = "#{swift_android}.artifactbundle"
 
   url "https://source.skip.tools/swift-android-toolchain/releases/download/#{swift_version}/#{artifact}.tar.gz"
@@ -22,7 +23,7 @@ cask "swift-android-toolchain@6.2" do
   depends_on cask: "android-ndk"
   depends_on macos: ">= :ventura"
 
-  swiftcmd = Pathname.new("~/Library/Developer/Toolchains/swift-#{swift_version}.xctoolchain/usr/bin/swift").expand_path
+  swiftcmd = Pathname.new("~/Library/Developer/Toolchains/swift-#{swift_version_id}.xctoolchain/usr/bin/swift").expand_path
   sdkpath = Pathname.new("~/Library/org.swift.swiftpm/swift-sdks/#{artifact}").expand_path
 
   postflight do
