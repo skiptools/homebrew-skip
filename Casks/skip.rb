0bde03c8f0b84a1ac53387f5226adbdbb8831700
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-12-21 19:10:56 -0500

    Release 1.6.32

--- Casks/skip.rb
+++ Casks/skip.rb
@@ -1,8 +1,11 @@
 cask "skip" do
   os macos: "macos", linux: "linux"
 
-  version "1.6.31"
-  sha256 :no_check
+  version "1.6.32"
+  sha256 arm:          "e3e6276b535b894ae06c7081fc979bc2cfac0b482bedab80b38a6ec319760702",
+         x86_64:       "e3e6276b535b894ae06c7081fc979bc2cfac0b482bedab80b38a6ec319760702",
+         arm64_linux:  "b1a461a6cd64c07866e969569f0997fc99beadb02868966a67051efbc62bb9c4",
+         x86_64_linux: "b1a461a6cd64c07866e969569f0997fc99beadb02868966a67051efbc62bb9c4"
 
   url "https://source.skip.tools/skip/releases/download/#{version}/skip-#{os}.zip",
       verified: "source.skip.tools/skip/"
