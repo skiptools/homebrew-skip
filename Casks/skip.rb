9a32e3c0e1e795b72fc9575906737b0649fc7966
Marc Prud'hommeaux <mwp1@cornell.edu>
2026-08-18 20:51:56 -0400

    Release 1.9.6

--- Casks/skip.rb
+++ Casks/skip.rb
@@ -1,14 +1,14 @@
 cask "skip" do
   os macos: "macos", linux: "linux"
 
-  version "1.9.5"
-  sha256 arm:          "3cf07daad97bf48145c4da0f91f5388e85a50a6016db2c35b0f6a84a6193a2df",
-         x86_64:       "3cf07daad97bf48145c4da0f91f5388e85a50a6016db2c35b0f6a84a6193a2df",
-         arm64_linux:  "17acfff4459c48b8245a8b77ce96a784c2a27fe4ff2e7e988128ff147085ed99",
-         x86_64_linux: "17acfff4459c48b8245a8b77ce96a784c2a27fe4ff2e7e988128ff147085ed99"
+  version "1.9.6"
+  sha256 arm:          "e0599cbc4ea3c23998b6918b8058e9b18c2faf4517c945bceb08034a3f1c05e1",
+         x86_64:       "e0599cbc4ea3c23998b6918b8058e9b18c2faf4517c945bceb08034a3f1c05e1",
+         arm64_linux:  "91ab743fb25de8d0914eac5e3618f3329a1ac49eb7bb052c0e5b69ec608ddaad",
+         x86_64_linux: "91ab743fb25de8d0914eac5e3618f3329a1ac49eb7bb052c0e5b69ec608ddaad"
 
-  url "https://source.skip.tools/skip/releases/download/#{version}/skip-#{os}.zip",
-      verified: "source.skip.tools/skip/"
+  url "https://github.com/skiptools/skip/releases/download/#{version}/skip-#{os}.zip",
+      verified: "github.com/skiptools/skip/"
   name "Skip"
   desc "Tool for creating and building universal swift apps"
   homepage "https://skip.dev"
